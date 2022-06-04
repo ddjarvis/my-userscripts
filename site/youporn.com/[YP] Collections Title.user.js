12d7ecfbbf7fb78b52ab344566038fec84bbb84a
David Jarvis <ddjarvis@up.edu.ph>
2022-06-04 18:32:54 +0800

    Create [YP] Collections Title.user.js

---
+++ site/youporn.com/[YP] Collections Title.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         [YP] Collections Title
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      /https:\/\/www\.youporn\.com\/collections\/videos\/\d+\//
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youporn.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+var ypcPrefix = '[YP Collections]';
+var ypcName = document.querySelector('div.collection-infos h4').innerText.replace('Collection: ','');
+var ypcPage = ypcPrefix + ' ' + ypcName;
+document.title = ypcPage;
+})();
