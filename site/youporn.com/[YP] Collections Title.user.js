1edab962b4ff58912fcd75a60825e52b20aed799
David Jarvis <ddjarvis@up.edu.ph>
2022-06-05 01:15:58 +0800

    Update [YP] Collections Title.user.js

--- site/youporn.com/[YP] Collections Title.user.js
+++ site/youporn.com/[YP] Collections Title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [YP] Collections Title
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youporn.com/collections/videos/*
